cb19d5d372645394eaa1734ba57d89d9ea713339
Rithvik <papatlarithvik@gmail.com>
2026-05-25 14:27:59 +0530

    Stop tracking unused router files

--- src/routes/library.tsx
+++
@@ -1,5 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { Stub } from "./my-simulations";
-export const Route = createFileRoute("/library")({
-  component: () => Stub("Library", "Browse the full simulation library."),
-});
--- src/routes/sandbox.$simulationId.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import SandboxPage from "@/sandbox/pages/SandboxPage";
-
-export const Route = createFileRoute("/sandbox/$simulationId")({
-  component: SandboxPage,
-});
